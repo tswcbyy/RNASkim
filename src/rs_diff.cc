5648b8fdfe5d52e60ee02d0922985c828831977f
Liuli Chen <liuli.chen@qilin.cs.ucla.edu>
2015-01-01 16:23:48 -0800

    add rs_diff.cc to test for differential expression given two distributions under different condtions for each transcript

---
+++ src/rs_diff.cc
@@ -0,0 +1,196 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+
+#include "gflags/gflags.h"
+#include "glog/logging.h"
+
+using namespace std;
+
+DEFINE_double(FDR, 0.05,
+             "Expected false discovery rate.");
+DEFINE_string(gibbs_file_conditionA, "",
+              "The path to the file that contains Gibbs Sampling results for condition A.");
+DEFINE_string(gibbs_file_conditionB, "",
+              "The path to the file that contains Gibbs Sampling results for condition B.");
+
+class transcript{
+public:
+    transcript(string tid, double meanA, double varianceA, double meanB, double varianceB){
+        this->tid = tid;
+        this->meanA = meanA;
+        this->varianceA = varianceA;
+        this->meanB = meanB;
+        this->varianceB = varianceB;
+        this->p_value = 1;
+        this->q_value = 1;
+        this->is_significant = false;
+    }
+    
+    string get_tid(){
+        return tid;
+    }
+    
+    double get_meanA(){
+        return meanA;
+    }
+    
+    double get_varianceA(){
+        return varianceA;
+    }
+    
+    double get_meanB(){
+        return meanB;
+    }
+    
+    double get_varianceB(){
+        return varianceB;
+    }
+    
+    double get_p_value(){
+        return p_value;
+    }
+    
+    double get_q_value(){
+        return q_value;
+    }
+    
+    bool get_significant(){
+        return is_significant;
+    }
+    
+    void set_p_value(double value){
+        p_value = value;
+    }
+    
+    void set_q_value(double value){
+        q_value = value;
+    }
+    
+    void set_significant(bool value){
+        is_significant = value;
+    }
+    
+private:
+    string tid;
+    double meanA;
+    double varianceA;
+    double meanB;
+    double varianceB;
+    double p_value;
+    double q_value;
+    bool is_significant;
+};
+
+vector<string> split(const string &s, char delim) {
+    vector<string> elems;
+    stringstream ss(s);
+    string item;
+    while (getline(ss, item, delim)) {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+void read_gibbs_result(vector<transcript*>& trans, string gibbs_fileA, string gibbs_fileB){
+    fstream istreamA(gibbs_fileA, ios::in);
+    fstream istreamB(gibbs_fileB, ios::in);
+    //count is the number of lines being read
+    int count = 0;
+    string lineA, lineB;
+    while (getline(istreamA, lineA)) {
+        ++count;
+        //line = transcript_id    mean_condition variance_condition
+        vector<string> tokensA = split(lineA, '\t');
+        getline(istreamB, lineB);
+        vector<string> tokensB = split(lineB, '\t');
+        LOG_IF(ERROR, tokensA[0] != tokensB[0])
+            << "Transcript ids in line" << count << " of two conditions are different.";
+        transcript* t = new transcript(tokensA[0], atof(tokensA[1].c_str()), atof(tokensA[2].c_str()),
+                                       atof(tokensB[1].c_str()), atof(tokensB[2].c_str()));
+        trans.push_back(t);
+    }
+//    trans.push_back(new transcript("test", 0, 0, 0, 0));
+//    trans.push_back(new transcript("test1", 10, 50, 0, 0));
+//    trans.push_back(new transcript("test2", 0, 0, 10, 50));
+//    trans.push_back(new transcript("test3", 10, 50, 11, 60));
+}
+
+void calc_p_value(transcript* t){
+    double meanA = t->get_meanA();
+    double varianceA = t->get_varianceA();
+    double meanB = t->get_meanB();
+    double varianceB = t->get_varianceB();
+    
+    if (meanA == 0 && meanB == 0){
+        t->set_p_value(1);
+    }
+    if (meanA != 0 && meanB != 0){
+        double t_value = (log(meanA) - log(meanB))/sqrt(varianceA/(meanA*meanA) + varianceB/(meanB*meanB));
+        t->set_p_value(erfc(abs(t_value)/sqrt(2)));
+    }
+    if (meanA == 0 && meanB != 0){
+        double p_comp = meanB/varianceB;
+        double r = meanB*meanB/(varianceB-meanB);
+        t->set_p_value(pow(p_comp, r));
+    }
+    if (meanA != 0 && meanB == 0){
+        double p_comp = meanA/varianceA;
+        double r = meanA*meanA/(varianceA-meanA);
+        t->set_p_value(pow(p_comp, r));
+    }
+}
+
+bool comp_trans(transcript* a, transcript* b){
+    return (a->get_p_value() < b->get_p_value());
+}
+
+void multiple_testing(vector<transcript*> trans, double FDR){
+    sort(trans.begin(), trans.end(), comp_trans);
+    int size = trans.size();
+    double min = trans[size-1]->get_p_value();
+    for (int i = size-1; i >= 0; --i){
+        double new_q = (double)size/(i+1)*trans[i]->get_p_value();
+        if (new_q < min) min = new_q;
+        trans[i]->set_q_value(min);
+        if (min <= FDR) {
+            trans[i]->set_significant(true);
+        }
+    }
+}
+
+int main(int argc, char *argv[]){
+    google::ParseCommandLineFlags(&argc, &argv, true);
+    
+    double FDR = FLAGS_FDR;
+    string gibbs_fileA = FLAGS_gibbs_file_conditionA;
+    string gibbs_fileB = FLAGS_gibbs_file_conditionB;
+    
+    vector<transcript*> trans;
+    read_gibbs_result(trans, gibbs_fileA, gibbs_fileB);
+    
+    for (transcript* t: trans){
+        calc_p_value(t);
+    }
+    
+    multiple_testing(trans, FDR);
+    
+    //output
+    cout << "transcript_id" << '\t' << "mean_conditionA" << '\t' << "variance_conditionA" << '\t'
+         << "mean_conditionB" << '\t' << "variance_conditionB" << '\t' << "p_value" << '\t'
+         << "q_value" << '\t' << "is_significant" << endl;
+    for (int i = 0; i < trans.size(); ++i){
+        transcript* t = trans[i];
+        cout << t->get_tid() << '\t' << t->get_meanA() << '\t' << t->get_varianceA() << '\t'
+             << t->get_meanB() << '\t' << t->get_varianceB() << '\t' << t->get_p_value() << '\t'
+             << t->get_q_value() << '\t';
+        if (t->get_significant()) {
+            cout << "yes";
+        }
+        else cout << "no";
+        cout << endl;
+    }
+}
